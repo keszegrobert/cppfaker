4d402242304eb71320b7a8982ef71dca41f52577
robert <keszegrobert@gmail.com>
2016-05-11 01:49:58 +0200

    cpp generator adjusted to the ast.json structure

--- cpp_generator.py
+++ cpp_generator.py
@@ -1,47 +1,64 @@
 
 class CppGenerator:
 
-    def generate_member(self, member):
+    def generate_field(self, member, indent):
         lines = []
-        if 'parameters' not in member:
-            lines.append('\t{} {}();'.format(
-                member['type'], member['name']
-            ))
-        else:
-            lines.append('\t{} {}({});'.format(
-                member['type'], member['name'], member['parameters']
-            ))
-        return '\n'.join(lines)
+        decl = member['declaration']
+        name = member['name']
+        lines.append('{}{} {};'.format(indent, decl, name))
+        return lines
 
-    def generate_class(self, fake):
+    def generate_method(self, member, indent):
         lines = []
-        lines.append('class {}{{'.format(fake['name']))
+        decl = member['declaration']
+        name = member['name']
+        splitpos = decl.find('(')
+        rtype = decl[0:splitpos]
+        desc = decl[splitpos: len(decl)]
+        lines.append('{}{}{}{};'.format(indent, rtype, name, desc))
+        return lines
+
+    def generate_class(self, fake, indent):
+        lines = []
+        lines.append('{}class {}{{'.format(indent, fake['name']))
         if 'members' in fake:
-            lines.append('public:')
-            for member in fake['members']:
-                lines.append(self.generate_member(member))
+            l = self.generate_inner(fake['members'], indent + '\t')
+            lines.extend(l)
         lines.append('}};\n'.format())
-        return '\n'.join(lines)
+        return lines
 
-    def generate_namespace(self, fake):
-        if 'name' in fake:
-            return 'namespace {}{{\n}};\n'.format(fake['name'])
-        else:
-            return 'namespace {{\n}};\n'.format()
+    def generate_namespace(self, fake, indent):
+        lines = []
+        lines.append('{}namespace {}{{'.format(indent, fake['name']))
+        if 'members' in fake:
+            lines.extend(self.generate_inner(fake['members'], indent + '\t'))
+        lines.append('}};\n'.format())
+        return lines
 
-    def generate_variable_declaration(self, fake):
-        return '{} {};\n'.format(fake['type'], fake['name'])
-
-    def generate(self, fakes):
+    def generate_inner(self, fakes, indent=''):
         generated = []
+        access = 'default'
         for fake in fakes:
             tp = ''
+            acc = access
+            if 'access' in fake:
+                acc = fake['access']
+            if acc != access:
+                generated.append('{}{}:'.format(indent, acc))
+                access = acc
+
             if 'type' in fake:
                 tp = fake['type']
             if tp == 'class':
-                generated.append(self.generate_class(fake))
+                generated.extend(self.generate_class(fake, indent))
             elif tp == 'namespace':
-                generated.append(self.generate_namespace(fake))
-            else:
-                generated.append(self.generate_variable_declaration(fake))
+                generated.extend(self.generate_namespace(fake, indent))
+            elif tp == 'method':
+                generated.extend(self.generate_method(fake, indent))
+            elif tp == 'field':
+                generated.extend(self.generate_field(fake, indent))
+        return generated
+
+    def generate(self, fakes):
+        generated = self.generate_inner(fakes, '')
         return '\n'.join(generated)
--- test_cpp_generator.py
+++ test_cpp_generator.py
@@ -22,8 +22,15 @@ class TestCppGenerator(TestCase):
         self.check_if_generated_code_for_fake_is(fakes, expected)
 
     def test_cpp_generator_should_generate_variable_declaration(self):
-        fakes = [{'type': 'int', 'name': 'ma'}]
-        expected = 'int ma;\n'
+        fakes = [{'type': 'field', 'declaration': 'int', 'name': 'ma'}]
+        expected = 'int ma;'
+        self.check_if_generated_code_for_fake_is(fakes, expected)
+
+    def test_cpp_generator_should_generate_function_declaration(self):
+        fakes = [
+            {'type': 'method', 'declaration': 'void (int)', 'name': 'foo'}
+        ]
+        expected = 'void foo(int);'
         self.check_if_generated_code_for_fake_is(fakes, expected)
 
     def test_cpp_generator_should_generate_members_if_provided(self):
@@ -31,12 +38,22 @@ class TestCppGenerator(TestCase):
             'type': 'class',
             'name': 'ShortStr',
             'members': [
-                {'type': 'void', 'name': 'Foo'},
-                {'type': 'void', 'name': 'Bar'}
+                {
+                    'type': 'method',
+                    'declaration': 'void ()',
+                    'name': 'Foo',
+                    'access': 'public'
+                },
+                {
+                    'type': 'method',
+                    'declaration': 'void ()',
+                    'name': 'Bar',
+                    'access': 'public'
+                }
             ]
         }]
         expected = 'class ShortStr{\n'\
-            'public:\n'\
+            '\tpublic:\n'\
             '\tvoid Foo();\n'\
             '\tvoid Bar();\n'\
             '};\n'
